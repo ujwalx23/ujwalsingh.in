19207af3f01f72b3386fed0a926039544a4e3d50
UJWAL SINGH <ujwalsingh164@gmail.com>
2026-01-08 12:58:05 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,11 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 sm:py-6 mt-3 sm:mt-4 fade-in-up stagger-7 bg-white/5 backdrop-blur-md rounded-t-2xl">
       <p className="text-center text-xs sm:text-sm text-blue-400">
-        Crafted with Blue by Ujwal
+          A space for my thoughts - Ujwal✨
       </p>
       <p className="text-center text-[10px] sm:text-xs text-muted-foreground mt-1">
-        Explore my projects, socials, and chat with Blue AI!
+ Explore my projects, socials, and conversations with my AI twin - and whatever I’m building next.
+</p>
       </p>
     </footer>
   );
