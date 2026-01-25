91ae3359cfe3f475e234ea70a015eaf3f1bee70d
UJWAL SINGH <ujwalsingh164@gmail.com>
2026-01-25 19:22:30 +0530

    Refactor main.tsx to include routing and helmet

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
 import "./index.css";
 
-// Cache bust: v2
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
